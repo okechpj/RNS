8318dc7ee93eb5a7545036a47ad3d6034e68d5a9
peter <okechpj@gmail.com>
2024-03-04 16:14:04 +0300

    add contact form functionality

---
+++ script.js
@@ -0,0 +1,10 @@
+function sendMail(){
+    let param = {
+        name : document.getElementById('name').value,
+        email : document.getElementById('email').value,
+        phone : document.getElementById('phone').value,
+        subject : document.getElementById('subject').value,
+        message : document.getElementById('message').value
+    }
+    emailjs.send("service_owqnqhi", "template_o0sbzrm", param).then(alert("Email has been sent!!"))
+}
