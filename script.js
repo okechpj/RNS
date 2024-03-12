e01ab6c52f7cfe712200b8dc92da892cfa5811b6
peter <okechpj@gmail.com>
2024-03-12 12:10:12 +0300

    header section

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
+
 function sendMail(){
     let param = {
         name : document.getElementById('name').value,
